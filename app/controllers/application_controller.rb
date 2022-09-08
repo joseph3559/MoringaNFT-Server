907f3f47a806524ef81325b13e40baba411884d7
joseph3559 <scottjoe3559@gmail.com>
2022-09-08 21:25:56 +0300

    add Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  set :default_content_type, "application/json"
+
+  get "/reviews" do
+      reviews = Review.all.order(:created_at)
+      reviews.to_json
   end
 
+  post "/reviews" do
+      review = Review.create(
+          title:params[:title],
+          content:params[:content]
+      )
+      review.to_json
+  end
+
+  delete "/reviews/:id" do
+      review = Review.find(params[:id])
+      review.destroy
+      review.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts "Seeding reviews..."
 Note.create(title: "Joseph Maina", content: "I am a software developer")
-Note.create(title: "Ruth Wekesa", content: "The first 5 days after the weekend are always the hardest")
+Note.create(title: "Ruth Wekesa", content: "MoringaNFT is the best market place on the internet")
 
 puts "Done seeding!"
