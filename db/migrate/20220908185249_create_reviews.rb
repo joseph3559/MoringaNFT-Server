70cb53e16c8f52cbb7fd33988a5305ad50b4eee2
joseph3559 <scottjoe3559@gmail.com>
2022-09-08 21:56:55 +0300

    Add tables

--- db/migrate/20220908185249_create_reviews.rb
+++ db/migrate/20220908185249_create_reviews.rb
@@ -1,6 +1,6 @@
-class CreateNotes < ActiveRecord::Migration[6.1]
+class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    create_table :notes do |t|
+    create_table :revies do |t|
       t.string :title
       t.string :content
       t.timestamps
