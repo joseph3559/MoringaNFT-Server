9f0883d8e2e1553e61686e5bcc1e5dce2c4281ff
joseph3559 <scottjoe3559@gmail.com>
2022-09-08 21:17:56 +0300

    Add Sinatra project structure

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+puts "Seeding reviews..."
+Note.create(title: "Joseph Maina", content: "I am a software developer")
+Note.create(title: "Ruth Wekesa", content: "The first 5 days after the weekend are always the hardest")
+
+puts "Done seeding!"
