9ed0f2b7f38e0cdb1dcde98b917aa41b9cf1e03b
joseph3559 <scottjoe3559@gmail.com>
2022-09-11 23:17:21 +0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "Seeding reviews..."
 Review.create(title: "Joseph Maina", content: "⭐⭐⭐⭐⭐This is a great app⭐⭐⭐⭐⭐")
 Review.create(title: "Ruth Wekesa", content: "⭐⭐⭐⭐⭐Most marketplaces rely on one or two cryptocurrency wallets. However, MoringaNFT supports several.")
-Review.create(title: "Mary Mwangi", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
-Review.create(title: "Peter Koo", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
+Review.create(title: "Mary Mwangi", content: "⭐⭐⭐⭐⭐ MoringaNFT is completely free to use initially. However, the platform does charge a 2.5% fee for each sale made on the marketplace. However, there are no monthly or annual fees")
+Review.create(title: "Peter Koo", content: "⭐⭐⭐⭐⭐ The one feature on MoringaNFT that truly sets it apart is gifting. For example, if you want to gift someone an NFT, all you need to do is send them the Ethereum address of a specific wallet.")
 Review.create(title: "John Kamau", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
 Review.create(title: "Jane Doe", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
 
