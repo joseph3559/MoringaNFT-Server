1608b0b2718ec50d47e93d736fbdaa0f111f42b9
joseph3559 <scottjoe3559@gmail.com>
2022-09-11 23:14:56 +0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "Seeding reviews..."
-Review.create(title: "Joseph Maina", content: "This is a great app⭐⭐⭐⭐⭐")
-Review.create(title: "Ruth Wekesa", content: "MoringaNFT⭐⭐⭐⭐⭐ is the best market place on the internet")
-Review.create(title: "Mary Mwangi", content: "MoringaNFT⭐⭐⭐⭐⭐ is the best market place on the internet")
-Review.create(title: "Peter Koo", content: "MoringaNFT⭐⭐⭐⭐⭐ is the best market place on the internet")
+Review.create(title: "Joseph Maina", content: "⭐⭐⭐⭐⭐This is a great app⭐⭐⭐⭐⭐")
+Review.create(title: "Ruth Wekesa", content: "⭐⭐⭐⭐⭐ MoringaNFT is the best market place on the internet")
+Review.create(title: "Mary Mwangi", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
+Review.create(title: "Peter Koo", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
 Review.create(title: "John Kamau", content: "MoringaNFT⭐⭐⭐⭐⭐ is the best market place on the internet")
 Review.create(title: "Jane Doe", content: "MoringaNFT⭐⭐⭐⭐⭐ is the best market place on the internet")
 
