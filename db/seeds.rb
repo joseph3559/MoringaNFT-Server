fd9be99dfff0fdd8819740ae07e1f316d3b3770c
joseph3559 <scottjoe3559@gmail.com>
2022-09-11 23:16:17 +0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts "Seeding reviews..."
 Review.create(title: "Joseph Maina", content: "⭐⭐⭐⭐⭐This is a great app⭐⭐⭐⭐⭐")
-Review.create(title: "Ruth Wekesa", content: "⭐⭐⭐⭐⭐ MoringaNFT is the best market place on the internet")
+Review.create(title: "Ruth Wekesa", content: "⭐⭐⭐⭐⭐Most marketplaces rely on one or two cryptocurrency wallets. However, MoringaNFT supports several.")
 Review.create(title: "Mary Mwangi", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
 Review.create(title: "Peter Koo", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
 Review.create(title: "John Kamau", content: "⭐⭐⭐⭐⭐ is the best market place on the internet")
